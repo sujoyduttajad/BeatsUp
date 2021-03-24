dd5dbf619c19d96491a57f6bbb46173da65ff9cd
sujoyduttajad <sujoyduttajad@gmail.com>
2021-03-24 12:55:33 +0530

    added animation & songs

--- src/data.js
+++ src/data.js
@@ -265,6 +265,90 @@ function chillHop() {
   duration: "2:18",
   album: "Anecdotes",
 },
+{
+  name: "Sunrise Hike",
+  cover: 
+      "https://i.scdn.co/image/ab67616d0000b273f3f3b79eee22fea56de44c43",
+  artist: "Ruck P",
+  audio: "https://mp3.chillhop.com/serve.php/?mp3=12133",
+  color: ['#D54948','#392E4C'],
+  id: uuidv4(),
+  active: false,  
+  duration: "2:20",
+  album: "Essentials Spring 2021",
+},
+{
+  name: "Coffee Break",
+  cover: 
+      "https://i.scdn.co/image/a107c2856862918d8da5249f77ec83bec388d4cf",
+  artist: "Ruck P",
+  audio: "https://mp3.chillhop.com/serve.php/?mp3=12135",
+  color: ['#FABB64','#2A5A9B'],
+  id: uuidv4(),
+  active: false,  
+  duration: "2:14",
+  album: "Nighttime Ramen",
+},
+{
+  name: "5 am",
+  cover: 
+      "https://i.scdn.co/image/ab67616d0000b273722571008347f858d8ef2947",
+  artist: "Ruck P, Shuko",
+  audio: "https://mp3.chillhop.com/serve.php/?mp3=10306",
+  color: ['#294B7B','#ABB5C1'],
+  id: uuidv4(),
+  active: false,  
+  duration: "2:12",
+  album: "Essentials Winter 2020",
+},
+{
+  name: "Destination",
+  cover: 
+      "https://i.scdn.co/image/ab67616d0000b27383843a83acf9cf314cbf4db5",
+  artist: "Ruck P",
+  audio: "https://mp3.chillhop.com/serve.php/?mp3=1956",
+  color: ['#E1C6A9','#43230F'],
+  id: uuidv4(),
+  active: false,  
+  duration: "4:42",
+  album: "Destination",
+},
+{
+  name: "Le Voyage Du Coeur",
+  cover: 
+      "https://i.scdn.co/image/ab67616d0000b27383843a83acf9cf314cbf4db5",
+  artist: "Ruck P",
+  audio: "https://mp3.chillhop.com/serve.php/?mp3=1959",
+  color: ['#E1C6A9','#43230F'],
+  id: uuidv4(),
+  active: false,  
+  duration: "2:30",
+  album: "Destination",
+},
+{
+  name: "Cascade",
+  cover: 
+      "https://i.scdn.co/image/ab67616d0000b273830bd373ee7338313033ec42",
+  artist: "Knowmadic",
+  audio: "https://mp3.chillhop.com/serve.php/?mp3=13091",
+  color: ['#E5B525','#65B592'],
+  id: uuidv4(),
+  active: false,  
+  duration: "2:45",
+  album: "Cascade",
+},
+{
+  name: "Traveling",
+  cover: 
+      "https://i.scdn.co/image/ab67616d0000b273b990c7fc4f939c3a94f819cb",
+  artist: "Anbuu",
+  audio: "https://mp3.chillhop.com/serve.php/?mp3=12137",
+  color: ['#41A89B','#EE977C'],
+  id: uuidv4(),
+  active: false,  
+  duration: "2:28",
+  album: "Essentials Spring 2021",
+},
   ];
 }
 
