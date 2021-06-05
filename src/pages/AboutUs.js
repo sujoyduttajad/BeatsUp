0b1ff62ec5d37a7cd08d032b5670fe2fd3df7c1d
sujoyduttajad <sujoyduttajad@gmail.com>
2021-06-05 15:22:29 +0530

    Added lottie animation in the about page

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,7 +3,7 @@ import Lottie from 'react-lottie';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar';
 import walkMan from '../lotties/lf30_editor_rmlzmksb.json'
-
+import circle from '../images/Ellipse7.svg'
 
 const AboutUs = () => {
 
@@ -13,7 +13,13 @@ const AboutUs = () => {
         autoplay: true,
         animationData: walkMan,
         rendererSettings: {
-          preserveAspectRatio: "xMidYMid slice"
+          preserveAspectRatio: "xMidYMid meet",
+          filterSize: {
+            width: '100%',
+            height: '90%',
+            x: '-10%',
+            y: '-6%',
+          }
         }
       };
 
@@ -21,13 +27,27 @@ const AboutUs = () => {
         <div>
             <Header />
             <aside><Sidebar /></aside>
-            <div className="animation">
-            <Lottie 
-                options={defaultOptions}
-                height={820}
-                width={560}
-            />
-            </div>
+                <section className="about-container">
+                    <div className="animation-container">
+                        {/* <img src={circle} alt="circle" /> */}
+                        <Lottie 
+                            className="man"
+                            options={defaultOptions}
+                            height={820}
+                            width={560}
+                        />
+                        
+                    </div>
+                <div className="about">
+                    {/* <p>ewBoxSize (svg renderer)
+                    if passed it will override the viewBox set by default on the animation
+                    </p> */}
+                </div>
+                 
+                </section>
+                <div>
+                   
+                </div>
             
         </div>
     )
