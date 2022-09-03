0bb30a0127ee1c9250078312807d7c11239c0cb1
Sujoy Dutta <33634147+sujoyduttajad@users.noreply.github.com>
2022-09-03 18:00:01 +0530

    Update Library.js

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,78 +1,88 @@
-import React, {useState, useContext} from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import LibrarySong from './LibrarySong';
+import React, { useState, useContext } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import LibrarySong from "./LibrarySong";
 // import { MusicContext } from '../context/musicContext';
-import data from '../data';
-
+import data from "../data";
 
 const useStyles = makeStyles({
-    root: {
-        width: '100%',
-    },  
-    container: {
-        maxHeight: 550,
-        padding: "1.2rem",
-        paddingTop: 0,
-    },
-    rows: {
-        backgroundColor: "#ffffff",
-    }
-  });
+  root: {
+    width: "100%",
+  },
+  container: {
+    maxHeight: 550,
+    padding: "1.2rem",
+    paddingTop: 0,
+  },
+  rows: {
+    backgroundColor: "#ffffff",
+  },
+});
 
- const Library = ({ 
-     songs, 
-     isPlaying, 
-     setIsPlaying, 
-     audioRef, 
-     currentSong, 
-    //  setSongs, 
-     setCurrentSong 
-    }) => {
+const Library = ({
+  songs,
+  isPlaying,
+  setIsPlaying,
+  audioRef,
+  currentSong,
+  //  setSongs,
+  setCurrentSong,
+}) => {
+  const classes = useStyles();
+  // const [isPlaying, setIsPlaying] = useContext(MusicContext);
+  console.log(songs);
+  const [songsLib, setSongsLib] = useState(data());
 
-    const classes = useStyles();
-    // const [isPlaying, setIsPlaying] = useContext(MusicContext);
-    console.log(songs);
-    const [songsLib, setSongsLib] = useState(data());
-
-    return (
-        <div className="library">
-            <TableContainer className={classes.container}>
-                <Table stickyHeader aria-label="sticky table">
-                <TableHead className="header">
-                    <TableRow>
-                        <TableCell align="center"><h3>Play/Pause</h3></TableCell>
-                        <TableCell align="center"><h3>Cover</h3></TableCell>
-                        <TableCell align="center"><h3>Title</h3></TableCell>
-                        <TableCell align="center"><h3>Artists</h3></TableCell>
-                        <TableCell align="center"><h3>Album</h3></TableCell>
-                        <TableCell align="center"><h3>Duration</h3></TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody className={`library-song`}>
-                        {songs.map((song) => (
-                            <LibrarySong 
-                                key={song.id} 
-                                song={song} 
-                                songsLib={songsLib} 
-                                setSongsLib={setSongsLib}
-                                currentSong={currentSong}                                 
-                                isPlaying={isPlaying}
-                                setIsPlaying={setIsPlaying}
-                                audioRef={audioRef}
-                                setCurrentSong={setCurrentSong}
-                            />
-                        ))}  
-                </TableBody>
-                </Table>
-            </TableContainer>
-        </div>
-    )
-}
+  return (
+    <div className="library">
+      <TableContainer className={classes.container}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead className="header">
+            <TableRow>
+              <TableCell align="center">
+                <h3>Play/Pause</h3>
+              </TableCell>
+              <TableCell align="center">
+                <h3>Cover</h3>
+              </TableCell>
+              <TableCell align="center">
+                <h3>Title</h3>
+              </TableCell>
+              <TableCell align="center">
+                <h3>Artists</h3>
+              </TableCell>
+              <TableCell align="center">
+                <h3>Album</h3>
+              </TableCell>
+              <TableCell align="center">
+                <h3>Duration</h3>
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody className={`library-song`}>
+            {songs.map((song) => (
+              <LibrarySong
+                key={song.id}
+                song={song}
+                songsLib={songsLib}
+                setSongsLib={setSongsLib}
+                currentSong={currentSong}
+                isPlaying={isPlaying}
+                setIsPlaying={setIsPlaying}
+                audioRef={audioRef}
+                setCurrentSong={setCurrentSong}
+              />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
 
 export default Library;
